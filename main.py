a33cb27e3087aa5f4009b3356055d0cbb81dbbb5
Dziema <motorollllo@gmail.com>
2021-06-18 12:57:45 +0300

    Second commit

--- main.py
+++ main.py
@@ -2,43 +2,53 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.color import Color
+import unittest
 
-search_term = "ASdsdsd"
-url = "https://www.pathofexile.com/trade"
+def search(browser,search_term):
+    time.sleep(3)
+    browser.find_element_by_css_selector('[placeholder="Search Items..."]').send_keys(search_term)
+    time.sleep(1)
+    browser.find_element_by_css_selector('[placeholder="Search Items..."]').send_keys(Keys.RETURN)
+    time.sleep(1)
+    browser.find_element_by_css_selector('[class="btn search-btn"]').click()
+    time.sleep(1)
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get(url)
-browser.implicitly_wait(30)
-
-time.sleep(3)
-browser.find_element_by_css_selector('[placeholder="Search Items..."]').send_keys(search_term)
-time.sleep(1)
-browser.find_element_by_css_selector('[placeholder="Search Items..."]').send_keys(Keys.RETURN)
-time.sleep(1)
-browser.find_element_by_css_selector('[class="btn search-btn"]').click()
-
-time.sleep(1)
-
-actual_text = browser.find_element_by_xpath('//*[@id="trade"]/div[6]/div[1]/h3').text
-expected_text = "No results found"
-time.sleep(1)
-assert actual_text == expected_text
-time.sleep(2)
+class TradeTest(unittest.TestCase):
+    def setUp(self) -> None:
+        url = "https://www.pathofexile.com/trade"
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.get(url)
 
 
-browser.get(url)
-time.sleep(2)
-search_term = "Chaos Orb"
+    def test_01(self):
+        search_term = "ASdsdsd"
+        search(self.browser,search_term)
+        actual_text = self.browser.find_element_by_xpath('//*[@id="trade"]/div[6]/div[1]/h3').text
+        expected_text = "No results found"
+        time.sleep(1)
+        assert actual_text == expected_text
+        time.sleep(2)
 
-browser.find_element_by_css_selector('[placeholder="Search Items..."]').send_keys(search_term)
-time.sleep(1)
-browser.find_element_by_css_selector('[placeholder="Search Items..."]').send_keys(Keys.RETURN)
-time.sleep(1)
-browser.find_element_by_css_selector('[class="btn search-btn"]').click()
-time.sleep(1)
-actual_text = browser.find_element_by_xpath('//*[@id="trade"]/div[6]/div[1]/h3').text
-expected_text = "Showing"
-assert expected_text in actual_text
 
-time.sleep(3)
-browser.close()
+    def test_02(self):
+        search_term = "Chaos Orb"
+        search(self.browser,search_term)
+        actual_text = self.browser.find_element_by_xpath('//*[@id="trade"]/div[6]/div[1]/h3').text
+        expected_text = "Showing"
+        assert expected_text in actual_text
+
+    def test_03(self):
+        search_term = "Headhunter Leather Belt"
+        search(self.browser,search_term)
+        rgb = self.browser.find_element_by_xpath('//*[@id="trade"]/div[6]/div[2]/div[1]/div[2]/div/div/div[1]/div[1]').value_of_css_property('color')
+        actual_color = Color.from_string(rgb).hex
+        expected_color = '#af6025'
+        assert actual_color == expected_color
+
+    def tearDown(self) -> None:
+            self.browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
